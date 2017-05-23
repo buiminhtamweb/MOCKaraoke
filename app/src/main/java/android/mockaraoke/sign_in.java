fd66e2cc1b5ebe6d741e13d156a0ffb6bec34840
Bùi Minh Tâm <buiminhtamweb@gmail.com>
2017-05-23 12:02:05 +0700

    Sign in

---
+++ app/src/main/java/android/mockaraoke/sign_in.java
@@ -0,0 +1,16 @@
+package android.mockaraoke;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by buimi on 5/23/2017.
+ */
+
+public class sign_in extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_in);
+    }
+}
